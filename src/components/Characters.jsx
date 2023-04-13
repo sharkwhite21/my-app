5a834184632498897a4029ad2cb6f210c339cda6
Marlon Baldovino <37963450+sharkwhite21@users.noreply.github.com>
2023-04-13 16:08:40 -0500

    Creando un custom Hook para los characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState , useReducer, useMemo, useRef, useCallback} from 'react';
+import React, { useState , useReducer, useMemo, useRef, useCallback} from 'react';
 import Search from './Search';
+import useCharacters from '../hooks/useCharacters';
 
 const initialState = {
   favorites: []
 }
 
+const API = 'https://rickandmortyapi.com/api/character/'
 const favoriteReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_TO_FAVORITE':
@@ -19,36 +21,18 @@ const favoriteReducer = (state, action) => {
 
 const Characters = ( props ) => {
 
-  const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
   const [search, setSearch] = useState('')
   const searchInput = useRef(null)
 
-  useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results));
-  }, []);
+  const characters = useCharacters(API)
 
   const handleClick = favorite => {
     dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
   }
-
-  // const handleSearch = (event) =>{
-  //   setSearch(event.target.value)
-  // }
-
-  // const handleSearch = () =>{
-  //   setSearch(searchInput.current.value)
-  // }
-
   const handleSearch = useCallback(() => {
     setSearch(searchInput.current.value)
   },[])
-
-  // const filteredUsers = characters.filter((user) => {
-  //   return user.name.toLowerCase().includes(search.toLowerCase())
-  // })
 
   const filteredUsers = useMemo(()=>
     characters.filter((user) => {
